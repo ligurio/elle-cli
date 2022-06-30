98bce850796ad94846a432264f9c3dd41b7226be
Sergey Bronnikov <estetus@gmail.com>
2022-06-30 17:31:05 +0300

    Bump Elle version
    Version of Elle has been updated to 0.1.5 [1]. Transactional models and
    anomalies synced with Elle.
    
    1. https://github.com/jepsen-io/elle/compare/v0.1.4...v0.1.5

--- project.clj
+++ project.clj
@@ -13,6 +13,6 @@
                  [org.clojure/tools.logging "1.1.0"]
                  [org.clojure/data.json "2.4.0"]
                  [spootnik/unilog "0.7.28"] ; required by elle
-                 [elle "0.1.4"]
+                 [elle "0.1.5"]
                  [jepsen "0.2.6"]
                  [knossos "0.3.8"]])
