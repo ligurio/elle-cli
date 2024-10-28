a4bfac31de32b212cf16b2abcaad1d4ff0943387
Sergey Bronnikov <estetus@gmail.com>
2024-10-28 13:41:43 +0300

    Bump Jepsen version
    Jepsen has been updated to version v0.3.7.
    See changelog [1] and separate changelogs for 0.3.5 [2], 0.3.6 [3]
    and 0.3.7 [4].
    
    1. https://github.com/jepsen-io/jepsen/compare/v0.3.4...v0.3.7
    2. https://github.com/jepsen-io/jepsen/releases/tag/v0.3.5
    3. https://github.com/jepsen-io/jepsen/releases/tag/v0.3.6
    4. https://github.com/jepsen-io/jepsen/releases/tag/v0.3.7

--- project.clj
+++ project.clj
@@ -14,5 +14,5 @@
                  [org.clojure/data.json "2.4.0"]
                  [spootnik/unilog "0.7.28"] ; required by elle
                  [elle "0.2.1"]
-                 [jepsen "0.3.4"]
+                 [jepsen "0.3.7"]
                  [knossos "0.3.9"]])
